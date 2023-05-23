65cb27e5a9c4c8ccd4787419d0ebc4fb860bde3f
AmauryMaros <maros.amaury@gmail.com>
2023-05-23 19:35:15 +0000

    link between search & result page / mytable2 with 5 element only

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect
-from database import games_from_db
+from flask import Flask, render_template, request
+from database import games_from_db, games_name_from_db
 
 app = Flask(__name__)
 
@@ -9,8 +9,15 @@ def index():
 
 @app.route("/result",methods=['POST'])
 def result():
+  games = games_from_db()
+  games_name = games_name_from_db()
   search = request.form['content']
-  return render_template('result.html', search = search)
+  correspondance = 0
+  if search in games_name:
+    correspondance = True
+  else :
+    correspondance = False
+  return render_template('result.html', games=games, games_name=games_name,search=search, correspondance = correspondance)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -8,12 +8,20 @@ engine = create_engine(db_connection_string,
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
+def games_name_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT Name FROM mytable2"))
+    games_name = []
+    for row in result.all():
+      games_name.append(row[0])
+      
+    return games_name
 
 def games_from_db():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from mytable"))
-
+    result = conn.execute(text("SELECT * FROM mytable2"))
     games = []
-    for row in result.all():
+    for row in result.all() :
       games.append(dict(zip(result.keys(), row)))
+      
     return games

3319245d1894abead1d5646065b2b0b963375ce0
AmauryMaros <maros.amaury@gmail.com>
2023-05-24 13:53:09 +0000

    everything on index.html + new function_from_db()

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask, render_template, request
-from database import games_from_db, games_name_from_db
+from flask import Flask, render_template, request, session
+from database import games_from_db, games_name_from_db,platform_from_db,metascore_from_db,userscore_from_db,url_from_db,date_from_db
 
 app = Flask(__name__)
+app.secret_key="secretkey"
+app.config["SESSION_COOKIE_NAME"] ="myCOOKIE"
 
-@app.route("/")
+@app.route("/",methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    session["games_name"] = games_name_from_db()
+    session["games"] = games_from_db()
+    session["platform"] = platform_from_db()
+    session["metascore"] = metascore_from_db()
+    session["userscore"] = userscore_from_db()
+    session["url"] = url_from_db()
+    session["date"] = date_from_db()
+    return render_template('index.html',games_name=session["games_name"], games=session["games"], platform=session["platform"],metascore=session["metascore"], userscore=session["userscore"], url=session["url"],date=session["date"])
+  
+@app.route('/choice', methods=['POST'])
+def choice():
+  session["game_choice"] = request.form['game_choice']
+  return render_template('index.html', game_choice=session["game_choice"], games_name=session["games_name"], games=session["games"], platform=session["platform"],metascore=session["metascore"], userscore=session["userscore"], url=session["url"],date=session["date"])
 
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+
+
+"""
 @app.route("/result",methods=['POST'])
 def result():
   games = games_from_db()
@@ -18,6 +37,4 @@ def result():
   else :
     correspondance = False
   return render_template('result.html', games=games, games_name=games_name,search=search, correspondance = correspondance)
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+"""
--- database.py
+++ database.py
@@ -16,6 +16,48 @@ def games_name_from_db():
       games_name.append(row[0])
       
     return games_name
+    
+def platform_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT Platform FROM mytable2"))
+    platform = []
+    for row in result.all() :
+      platform.append(row[0])
+    return platform
+
+def date_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT Date FROM mytable2"))
+    date = []
+    for row in result.all() :
+      date.append(row[0])
+    return date
+
+
+def metascore_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT Metascore FROM mytable2"))
+    metascore = []
+    for row in result.all() :
+      metascore.append(row[0])
+    return metascore
+
+def userscore_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT Userscore FROM mytable2"))
+    userscore = []
+    for row in result.all() :
+      userscore.append(row[0])
+    return userscore
+
+def url_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT URL FROM mytable2"))
+    URL = []
+    for row in result.all() :
+      URL.append(row[0])
+    return URL
+
 
 def games_from_db():
   with engine.connect() as conn:
@@ -23,5 +65,5 @@ def games_from_db():
     games = []
     for row in result.all() :
       games.append(dict(zip(result.keys(), row)))
-      
     return games
+
